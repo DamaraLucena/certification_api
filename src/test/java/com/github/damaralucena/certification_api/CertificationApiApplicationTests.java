be1e0ff28fecee3cf8442eef20ebc43934591506
damaralucena <damara.ti.dev@gmail.com>
2024-02-05 22:05:35 -0300

    Initialized project

---
+++ src/test/java/com/github/damaralucena/certification_api/CertificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.damaralucena.certification_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
