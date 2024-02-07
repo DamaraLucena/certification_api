dcb8e20df37bbf5d2bb7b233bd85b4467fd93c04
damaralucena <damara.ti.dev@gmail.com>
2024-02-06 22:39:55 -0300

    Update entities

--- src/main/java/com/github/damaralucena/certification_api/modules/students/entities/AnswersCertifications.java
+++ src/main/java/com/github/damaralucena/certification_api/modules/students/entities/AnswersCertifications.java
@@ -1,21 +1,49 @@
 package com.github.damaralucena.certification_api.modules.students.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "answers_certification_students")
 public class AnswersCertifications {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(name = "certification_id")
   private UUID certificationID;
+
+  @ManyToOne()
+  @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+  private CertificationStudent certificationStudent;
+
+  @Column(name = "student_id")
   private UUID studentID;
+
+  @ManyToOne
+  @JoinColumn(name = "student_id", insertable = false, updatable = false)
+  private Student student;
+
+  @Column(name = "question_id")
   private UUID questionID;
+
+  @Column(name = "answer_Id")
   private UUID answerID;
+
+  @Column(name = "is_correct")
   private boolean isCorrect;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
--- src/main/java/com/github/damaralucena/certification_api/modules/students/entities/CertificationStudent.java
+++ src/main/java/com/github/damaralucena/certification_api/modules/students/entities/CertificationStudent.java
@@ -1,22 +1,45 @@
 package com.github.damaralucena.certification_api.modules.students.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudent {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private UUID studentID;
+
+  @Column(length = 100)
   private String technology;
+
+  @Column(length = 10)
   private int grade;
+
+  @JoinColumn(name = "student_id")
+  private UUID studentId;
+
+  @ManyToOne
+  @JoinColumn(name = "student_id", insertable = false, updatable = false)
+  private Student student;
+
+  @OneToMany
+  @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
   List<AnswersCertifications> answersCertifications;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 
 }
